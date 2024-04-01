5e2adb43cc1e3299bdc3fab08dbb9ae29423e6fe
chepkaroi <amontoo53@gmail.com>
2024-04-01 18:22:47 +0300

    new design

--- src/component/landing.jsx
+++ src/component/landing.jsx
@@ -9,15 +9,15 @@ class Landing extends Component {
         return (
             <div className='w-full  flex text-black justify-between px-16 cursor-pointer'>
                <div className='mt-[10%]'>
-               <h1 className='p-5 font-semi-bold  text-5xl'>HELLO I<span className='font-bold text-6xl text-[#1DB954]'>`</span>M 👋 </h1>
+               <h1 className='p-5 font-semi-bold  text-5xl'>HELLO I<span className='font-bold text-6xl text-[#005d99]'>`</span>M 👋 </h1>
                <h1 className='p-5 font-semi-bold mt-3text-bold  text-5xl'>  AMON TOO</h1>
-                <p className='p-5  justify-center text-3xl'>I am an expert from <br />
-                <span className='font-bold text-4xl text-[#1DB954]'>Kenya</span> in Developing $ <br />
+                <p className='p-5 text-[#cccccc] justify-center text-3xl'>I am an expert from <br />
+                <span className='font-bold text-4xl text-[#005d99]'>Kenya</span> in Developing $ <br />
                 Designing awesome web Apps</p>
 
-                <div className=''><button className='text-white mx-4 my-5 px-16 rounded-3xl py-4 hover:bg-[#53da82]  bg-[#1DB954]'>Hire Me!</button></div>
+                <div className=''><button className='text-white mx-4 my-5 px-16 rounded-3xl py-4 hover:bg-[#005c99bb]  bg-[#005d99]'>Hire Me!</button></div>
                </div>
-              <div><img src={Image1} alt="Me" className='w-[55%] mx-[30%] my-[10%] hover:rotate-[-2deg] hover:w-[56%]  border-[#1DB954] border-[3px] rounded-[10%]' /></div>
+              <div><img src={Image1} alt="Me" className='w-[55%] mx-[30%] my-[10%] hover:rotate-[-2deg] hover:w-[56%]  border-[#005d99] border-[3px] rounded-[10%]' /></div>
            </div>
         );
     }   
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBriefcase, faCalendarDays, faEnvelope, faLocationDot, faPhone, faSignal, faSun, faUser } from "@fortawesome/free-solid-svg-icons";
-import { faFacebook, faTwitter, faIntercom, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faHouse, faBriefcase, faPhone, faUser } from "@fortawesome/free-solid-svg-icons"; // Import only necessary icons
+import Image1 from  "../images/img1.jpg";
 
 class Home extends Component {
     render() {
         return (
-           <div className='w-full  flex text-black justify-between px-16 cursor-pointer'>
-            <div className='p-4 text-[#1DB954] font-bold text-2xl '>
-                <p>Amon Too</p>
+           <div className='border-[2px] cursor-cell p-3 max-w-[400px] rounded-[30px] border-[#005d99] flex-col align-middle justify-center '>
+            <div><img src={Image1} alt="Me" className='w-[250px] h-[300px] rounded-[10%] border-none mx-auto' /></div>
+            <div className='flex align-middle justify-center pt-8 text-2xl font-bold'>
+            <h1>Amon Too</h1>
             </div>
-            <div>
-                <ul className='flex items-center'>
-                    <li className='font-semi-bold hover:text-[#1DB954] p-4'><a href="#">Home</a></li>
-                    <li className='font-semi-bold hover:text-[#1DB954]  p-4'><a href="#">About</a></li>
-                    <li className='font-semi-bold hover:text-[#1DB954]  p-4'><a href="#">What I Do</a></li>
-                    <li className='font-semi-bold hover:text-[#1DB954]  p-4'><a href="#">Recent Work</a></li>
-                    <li className='font-semi-bold hover:text-[#1DB954]  p-4'><a href="#">Contact</a></li>
+            <div className='flex align-middle justify-center  pt-8 text-1xl font-bold'>
+            <h1 className='text-[#cccccc]'> Ux/Ui Designer $ Developer</h1>
+            </div>
+            <div className='w-[250px] h-auto my-[10%] pt-[10%] rounded-[10%] border-t-[1px] border-b-[1px] mx-auto'>
+            <div className=' flex p-[10%] hover:bg-[#282c34] hover:rounded-[10px]'>
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={faHouse} className="fa-solid text-[#005d99]"/> {/* Display house icon */}
+                        <a href="#" className=" mx-[50px]">Home</a>
+                    </li>
                 </ul>
             </div>
-            <div className=''><button className='text-white px-6 py-3 rounded-3xl bg-[#1DB954] hover:bg-[#53da82]'>Hire Me!</button></div>
+            <div className=' flex p-[10%] hover:bg-[#282c34] hover:rounded-[10px]'>
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={faBriefcase} className="fa-solid text-[#005d99]"/> {/* Display briefcase icon */}
+                        <a href="#" className=" mx-[50px]">About</a>
+                    </li>
+                </ul> 
+            </div>
+            <div className='flex p-[10%] hover:bg-[#282c34] hover:rounded-[10px]'>
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={faBriefcase} className="fa-solid text-[#005d99]"/> {/* Display briefcase icon */}
+                        <a href="#" className=" mx-[50px]">Resume</a>
+                    </li>
+                </ul>
+            </div>
+            <div className=' flex p-[10%] hover:bg-[#282c34] hover:rounded-[10px]'>
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={faPhone} className="fa-solid text-[#005d99]"/> {/* Display phone icon */}
+                        <a href="#" className=" mx-[50px] ">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div className='w-auto flex justify-center'>
+            <button className='bg-[#005d99] p-2 w-[50%] hover:bg-[#005c99a6] flex justify-center rounded-[10px] border-none my-5'>Hire Me!</button>
+            </div>
+            </div>
            </div>
         );
     }   
